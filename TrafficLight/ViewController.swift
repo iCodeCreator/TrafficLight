c02aaedad8bcfb623810d18610482ff10bb9cbff
abdul'Karim <iCodeCreator>
2024-03-13 14:52:57 +0300

    TrafficLight

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+final class ViewController: UIViewController {
+
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var switchTrafficLightButton: UIButton!
+    private var currentLight = CurrentLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchTrafficLightButton.layer.cornerRadius = 13
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
     }
-
-
+    
+    @IBAction func switchTrafficLightDidTapped(_ sender: UIButton) {
+        if switchTrafficLightButton.currentTitle == "START" {
+            switchTrafficLightButton.setTitle("NEXT", for: .normal)
+        }
+        switch currentLight {
+        case .red:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            currentLight = .yellow
+        case .yellow:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            currentLight = .green
+        case .green:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            currentLight = .red
+        }
+    }
+    
 }
+
 
